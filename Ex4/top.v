4aacc3f6a78917a4a939725b7486be87f39a9273
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-15 11:27:16 +0000

    partial solution to ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Julie Yeow
+// Date: 15/06/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,37 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module lights (
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0] colour
+    );
+
+always @ (posedge clk)
+
+if (rst)
+    colour= 3'b001;
+
+else
+
+    if (button)
+    case (colour)
+	3'b001: colour= 3'b010 ;
+	3'b010: colour= 3'b011 ;     
+        3'b011: colour= 3'b100 ;
+	3'b100: colour= 3'b101 ;
+	3'b101: colour= 3'b110 ;
+	default: colour= 3'b001 ;
+    endcase
+
+    else
+    case (colour)
+        3'b000: colour= 3'b001;
+        3'b111: colour= 3'b001 ;
+        default: colour= colour; 
+    endcase
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,48 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Julie Yeow
+// Date: 15/06/21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+
+parameter CLK_PERIOD=10; // clock period set to 10 sim ticks
+
+// registers and wires
+reg rst, clk, button;
+wire [2:0] colour;
+
+// clock generation
+initial
+begin 
+    clk= 0;
+    forever
+      # (CLK_PERIOD/2) clk=~clk;
+end 
+
+// user logic
+initial
+begin
+    rst=1;
+    button=1;
+
+    # (CLK_PERIOD*5) rst=0;
+
+end
+
+// finish test
+initial
+begin
+    # (CLK_PERIOD*60);		//sim time 600ns
+    $finish;
+end
+
+// instantiate lights module
+
+lights top(.clk(clk), .rst(rst), .button(button), .colour(colour));
+
+endmodule 
