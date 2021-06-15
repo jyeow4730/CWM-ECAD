b85a982320c562b150261cde40c9428a4d633b9c
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-15 14:12:13 +0000

    Complete solution to ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -17,7 +17,8 @@ module top_tb(
 parameter CLK_PERIOD= 10;	// clock period set to 10 sim ticks
 
 //Todo: Registers and wires
-reg clk, rst, change, on_off;
+reg clk, rst, change, on_off,err;
+reg [7:0] counter_out_prev;
 wire [7:0] counter_out;
 
 //Todo: Clock generation
@@ -34,24 +35,53 @@ begin
     rst=1;
     change=0;
     on_off=0;
+    counter_out_prev= 0;
+    err=0;
 
-    # (CLK_PERIOD*4) rst=0;
+# (CLK_PERIOD)
 
-    # (CLK_PERIOD*5) change=1;
+if (counter_out !=0)
+begin 
+  $display("Test failed!");
+  err=1;
+end
 
-    # (CLK_PERIOD*5) on_off=1;
+rst=0;
 
-    # (CLK_PERIOD*10) on_off=0;
+# (CLK_PERIOD)
+if (counter_out !=0)
+begin 
+  $display("Test failed!");
+  err=1;
+end
 
-    # (CLK_PERIOD*20) on_off=1;
+change=1;
 
-    # (CLK_PERIOD*10) rst=1;
+forever
+begin
+
+# (CLK_PERIOD)
+
+counter_out_prev= on_off? counter_out_prev + 1: counter_out_prev -1;
+
+if (counter_out_prev!= counter_out)
+  begin
+  $display("Test failed!");
+  err=1;
+  end
+
+if (counter_out== 8'hf0)
+    on_off = ~on_off;
+end
+
 end
     
 //Todo: Finish test, check for success
 initial
 begin
     # (CLK_PERIOD*60);		//sim time 600ns
+    if (err==0)
+       $display("***TEST PASSED! :) ***");
     $finish;
 end
 
