f55b66e1dd550c224ca712cf92150983fccfcc17
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-16 09:32:20 +0000

    work on ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Julie Yeow
+// Date: 16/06/21
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,36 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module airconditioning(
+       input clk,
+       input [4:0] temperature,
+       output heating, cooling
+       );
+
+reg [1:0] state;
+wire heating, cooling;
+
+always @ (posedge clk)
+
+  state= {heating, cooling};
+  
+  if (state== 2'b00)		// idle state
+      assign heating = (temperature <= 5'b10010) ? 1: 0;
+      assign cooling = (temperature >= 5'b10110) ? 1: 0;
+
+  if (state== 2'b01)		// cooling state
+      assign heating = 0;
+      assign cooling = (temperature <= 5'b10100) ? 1: 0;
+
+  if (state== 2'b10)		// heating state
+      assign heating = (temperature >= 5'b10100) ? 1: 0;
+      assign cooling=0;
+
+  else   			// forbidden state
+     assign heating=0;
+     assign cooling=0;
+
+endmodule
+ 
