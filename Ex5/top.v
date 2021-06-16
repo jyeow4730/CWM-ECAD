08e4acda49ba28eb5f77f30a91e704f00f48c261
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-16 10:32:59 +0000

    working solution to ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -24,24 +24,34 @@ reg [1:0] state;
 wire heating, cooling;
 
 always @ (posedge clk)
+begin
+    
+  if (state== 2'b00)		// idle state 
+      begin
+       state = (temperature <= 5'b10010) ? 2'b10: 2'b00;	//turn on heating if T<=18
+       state = (temperature >= 5'b10110) ? 2'b01: 2'b00;	// turn on cooling if T>= 22
+      end
 
-  state= {heating, cooling};
-  
-  if (state== 2'b00)		// idle state
-      assign heating = (temperature <= 5'b10010) ? 1: 0;
-      assign cooling = (temperature >= 5'b10110) ? 1: 0;
-
+  else 
   if (state== 2'b01)		// cooling state
-      assign heating = 0;
-      assign cooling = (temperature <= 5'b10100) ? 1: 0;
+      begin
+       state = (temperature <= 5'b10100) ? 2'b00: 2'b01;	//turn off cooling if T<= 20
+      end
 
+  else
   if (state== 2'b10)		// heating state
-      assign heating = (temperature >= 5'b10100) ? 1: 0;
-      assign cooling=0;
+      begin
+       state = (temperature >= 5'b10100) ? 2'b00: 2'b10;	//turn off heating if T>= 20
+      end
 
-  else   			// forbidden state
-     assign heating=0;
-     assign cooling=0;
+  else
+     begin   			// forbidden state
+      state= 2'b00;
+     end
+end
+
+assign heating= state[1];
+assign cooling= state[0]; 
 
 endmodule
  
