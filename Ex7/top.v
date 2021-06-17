9c43d041fa5c0808dec51938da6f5273f2ad2fb7
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-17 15:04:14 +0000

    working solution to ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7 - Lights Selector
-// Student Name:
-// Date: 
+// Student Name: Julie Yeow
+// Date: 17/06/21
 //
 //  Description: In this exercise, you need to implement a selector between RGB 
 // lights and a white light, coded in RGB. If sel is 0, white light is used. If
@@ -13,5 +13,30 @@
 //  outputs:
 //           light [23:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module lights_selector(
+      input clk, sel, rst, button,
+      output [23:0] light
+      );
+
+//registers and wires
+wire [2:0] colour;
+wire enable;
+wire [23:0] rgb;
+wire [23:0] white;
+
+assign enable= ~rst;
+assign white= 24'hFFFFFF;
+
+// instantiate modules
+lights LED_lights(.clk(clk), .rst(rst), .button(button), .colour(colour));
+
+RGB rgb_converter(.clk(clk), .enable(enable), .colour(colour), .rgb(rgb));
+
+mux multiplexer(.white(white), .rgb(rgb), .sel(sel), .light(light));
+
+endmodule
+
 
 
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -1,9 +1,70 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #7 - Lights Selector
-// Student Name:
-// Date: 
+// Student Name: Julie Yeow
+// Date: 17/06/21
 //
 // Description: A testbench module to test Ex7 - Lights Selector
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+
+parameter CLK_PERIOD=10; // clock period set to 10 sim ticks
+
+// registers and wires
+reg clk, rst, sel, button, err;
+wire [23:0] light;
+
+// clock generation
+initial
+begin 
+    clk= 1;
+    forever
+      # (CLK_PERIOD/2) clk=~clk;
+end 
+
+// user logic
+initial
+begin
+  sel=0;
+  rst=1;
+  button=0;
+  err=0;
+
+// check for white light (sel=0)
+  # (CLK_PERIOD)
+  if (light!= 24'hFFFFFF)
+  begin
+      $display("***TEST FAILED!:( ***");
+      err=1;
+    end
+
+# (CLK_PERIOD*2)
+  sel=1;	//switch to RGB mode
+
+// test reset (rst=1)
+  if (light!= 24'h0000FF)
+  begin
+      $display("***TEST FAILED!:( ***");
+      err=1;
+   end
+
+
+
+end
+
+
+// finish test and check for success
+initial
+begin
+    # (CLK_PERIOD*60);		//sim time 600ns
+    if (err==0)
+       $display("***TEST PASSED! :) ***");
+    $finish;
+end
+
+// instantiate lights selector module
+lights_selector top(.clk(clk), .sel(sel), .rst(rst), .button(button), .light(light));
+
+endmodule
